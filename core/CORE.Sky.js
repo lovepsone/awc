8f8d2b6fc259343a65f4c7edc1f0ab7ca58c2463
lovepsone <lovepsone@gmail.com>
2017-01-13 14:17:53 +0300

    [020] Fixed deplicated SKY

--- core/CORE.Sky.js
+++ core/CORE.Sky.js
@@ -2,9 +2,7 @@
 var CORE  = CORE || {};
 CORE.Sky = CORE.Sky || {};
 
-CORE.Sky.loader = new THREE.JSONLoader();
-CORE.Sky.Loaded = false;
-//времмено
+CORE.Sky.loader = new THREE.TextureLoader();
 
 CORE.Sky.Load = function(_scene)
 {
@@ -12,14 +10,19 @@ CORE.Sky.Load = function(_scene)
 	var directions = ['px', 'nx', 'py', 'ny', 'pz', 'nz'];
 	var materialArray = [];
 	for (var i = 0; i < 6; i++)
-		materialArray.push( new THREE.MeshBasicMaterial({
-		map: THREE.ImageUtils.loadTexture('textures/skybox/2/sky_cube_0'+(i+1)+'.png'),
-		side: THREE.BackSide
-	}));
+		CORE.Sky.LoadTexture(materialArray, i);
 
-	var skyMaterial = new THREE.MeshFaceMaterial( materialArray );
-	var skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
+	var skyMaterial = new THREE.MultiMaterial(materialArray);
+	var skyBox = new THREE.Mesh(skyGeometry, skyMaterial);
 	skyBox.position.y = 2400;
 	skyBox.rotation.y = Math.PI/4;
 	_scene.add(skyBox);
+}
+
+CORE.Sky.LoadTexture = function(matArr, index)
+{
+	CORE.Sky.loader.load('textures/skybox/2/sky_cube_0'+(index+1)+'.png', function(texture)
+	{
+		matArr.push(new THREE.MeshBasicMaterial({map: texture, side: THREE.BackSide}));
+	});
 }
