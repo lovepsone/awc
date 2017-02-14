a4e2caf1aea66471a467669fcac3b2f2a72d9e0f
lovepsone <lovepsone@gmail.com>
2017-02-14 18:15:19 +0300

    [070] rewritten by class

--- core/CORE.LoaderMap.js
+++ core/CORE.LoaderMap.js
@@ -1,94 +1,94 @@
+"use strict";
+
 var CORE = CORE || {};
 CORE.LoaderMap = CORE.LoaderMap || {};
 
-CORE.LoaderMap.manager		= new THREE.LoadingManager();
-CORE.LoaderMap.loader		= new THREE.JSONLoader(CORE.LoaderMap.manager);
-CORE.LoaderMap.Static		= {};
-CORE.LoaderMap.MapMesh		= [];
-
-CORE.LoaderMap.LoadObj = false;
-CORE.LoaderMap.LoadFinish = false;
-
-CORE.LoaderMap.manager.onProgress = function(item, loaded, total)
+CORE.LoaderMap = function(_scene, _mp, _mps)
 {
-	var v = Math.round(loaded*100/total).toFixed(0);
-	//HANDLER.Interface.pLoaderMaps.val('10');
-	$("#pLoaderMaps").val(v);
-	$('.pvLoaderMaps').html(v + '%');
-}
+	var manager = new THREE.LoadingManager();
+	var loader = new THREE.JSONLoader(manager);
 
-CORE.LoaderMap.manager.onLoad = function()
-{
-	CORE.LoaderMap.LoadFinish = true;
-	HANDLER.Interface.LoaderMaps.hide();
-	HANDLER.Interface.CORE.show();
-	console.log(1);
-}
+	var scene = _scene;
 
-CORE.LoaderMap.LoadMap = function(_scene)
-{
-	
-	for (var i = 0; i < CORE.Object3D.mps[1].length; i++)
+	this.mp = _mp;
+	this.mps = _mps;
+	this.MapMesh = [];
+
+	manager.onProgress = function(item, loaded, total)
 	{
-		CORE.LoaderMap.LoadSquareMap(_scene, CORE.Object3D.mp[CORE.Object3D.mps[1][i]].path, CORE.Object3D.mp[CORE.Object3D.mps[1][i]].name, CORE.Object3D.mp[CORE.Object3D.mps[1][i]].position);
+		var v = Math.round(loaded*100/total).toFixed(0);
+		$("#pLoaderMaps").val(v);
+		$('.pvLoaderMaps').html(v + '%');
 	}
-}
 
-CORE.LoaderMap.LoadSquareMap = function(_scene, patch, name, pos)
-{
-	CORE.LoaderMap.loader.load(patch, function(geometry, materials)
+	manager.onLoad = function()
 	{
-		var texture = materials[0].map;
-		texture.wrapS = texture.wrapT = THREE.RepeatWrapping; 
-		texture.repeat.set(1, 1);
-		geometry.computeVertexNormals();
-		geometry.mergeVertices();
-		geometry.sortFacesByMaterialIndex();
-		
-		for (var j = 0; j < materials.length; j++)
+		HANDLER.Interface.LoaderMaps.hide();
+		HANDLER.Interface.CORE.show();
+	}
+
+	this.LoadSquareMap = function(_scene, _group, _patch, _name, _position)
+	{
+		loader.load(_patch, function(geometry, materials)
 		{
-			materials[j].map.wrapS = THREE.RepeatWrapping;
-			materials[j].map.wrapT = THREE.RepeatWrapping;
-			materials[j].map.anisotropy = 4;
-			materials[j].needsUpdate = true;
-			if (materials[j].name == 'alpha_map')
+			var texture = materials[0].map;
+			texture.wrapS = texture.wrapT = THREE.RepeatWrapping; 
+			texture.repeat.set(1, 1);
+			geometry.computeVertexNormals();
+			geometry.mergeVertices();
+			geometry.sortFacesByMaterialIndex();
+			
+			for (var j = 0; j < materials.length; j++)
 			{
-				materials[j].map.repeat.set(1, 1);
-				materials[j].transparent = true;
-				materials[j].alphaMap.wrapS = THREE.RepeatWrapping;
-				materials[j].alphaMap.wrapT = THREE.RepeatWrapping;
-				materials[j].side = THREE.DoubleSide;
-				materials[j].alphaTest = 0.5;
+				materials[j].map.wrapS = materials[j].map.wrapT = THREE.RepeatWrapping;
+				materials[j].map.anisotropy = 4;
+				materials[j].needsUpdate = true;
+				if (materials[j].name == 'alpha_map')
+				{
+					materials[j].map.repeat.set(1, 1);
+					materials[j].transparent = true;
+					materials[j].alphaMap.wrapS = materials[j].alphaMap.wrapT = THREE.RepeatWrapping;
+					materials[j].side = THREE.DoubleSide;
+					materials[j].alphaTest = 0.5;
+				}
+				else if (materials[j].name == 'duable_map')
+				{
+					materials[j].map.repeat.set(1, 1);
+					materials[j].side = THREE.DoubleSide;
+				}
+				else if (materials[j].name == 'prop_fake_occ_S')
+				{
+					materials[j].visible = false;
+				}
+				else if (materials[j].name == 'trees_fuflo_S' || materials[j].name == 'prop_fake_kollision_S')
+				{
+					materials[j].visible = false;
+				}
 			}
-			else if (materials[j].name == 'duable_map')
+			var mesh = new THREE.Mesh(geometry, new THREE.MultiMaterial(materials));
+			mesh.position.copy(_position);
+			mesh.name = _name;
+			var n = _name.split("_");
+			mesh.visible = false;
+			// временная затычка
+			if (n[1] == 'c3' || n[1] == 'c4' || n[1] == 'c2')
 			{
-				materials[j].map.repeat.set(1, 1);
-				materials[j].side = THREE.DoubleSide;
+				mesh.visible = true;
 			}
-			else if (materials[j].name == 'prop_fake_occ_S')
-			{
-				materials[j].visible = false;
-			}
-			else if (materials[j].name == 'trees_fuflo_S' || materials[j].name == 'prop_fake_kollision_S')
-			{
-				materials[j].visible = false;
-			}
-		}
-		var mesh = new THREE.Mesh(geometry, new THREE.MultiMaterial(materials));
-		mesh.position.copy(pos);
-		mesh.name = name;
-		var n = name.split("_");
-		mesh.visible = false;
-		// временная затычка
-		if (n[1] == 'c3')
+			mesh.matrixAutoUpdate = false;
+			// --
+			// требуется загрузить сетку для физики (физический движок пока не встроен)
+			// ---
+			_group.push(mesh);
+			_scene.add(mesh);
+		});		
+	};
+	
+	this.LoadMap = function()
+	{
+		for (var i = 0; i < this.mps.length; i++)
 		{
-			mesh.visible = true;
+			this.LoadSquareMap(scene, this.MapMesh, this.mp[this.mps[i]].path, this.mp[this.mps[i]].name, this.mp[this.mps[i]].position);
 		}
-		mesh.matrixAutoUpdate = false;
-		// --
-		// требуется загрузить сетку для физики (физический движок пока не встроен)
-		// ---
-		CORE.LoaderMap.MapMesh.push(mesh);
-		_scene.add(mesh);
-	});
-}
+	}
+};
